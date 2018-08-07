8938eccd35c279f430a49291f157fe24b3020b96
eyjhbb@gmail.com <eyjhbb@gmail.com>
2018-08-07 12:33:42 +0200

    added basic test for vbox

---
+++ virtual/vbox/vbox_test.go
@@ -0,0 +1,94 @@
+package vbox_test
+
+import (
+	"fmt"
+	"testing"
+    "os/exec"
+
+    "github.com/aau-network-security/go-ntp/virtual/vbox"
+    "github.com/rs/zerolog/log"
+    "github.com/stretchr/testify/assert"
+)
+
+const (
+	vboxBin     = "VBoxManage"
+	vboxModVM   = "modifyvm"
+	vboxStartVM = "startvm"
+	vboxCtrlVM  = "controlvm"
+)
+
+
+func init() {
+    fmt.Println("Init function!")
+    log.Debug().Msg("Init..")
+}
+
+func execute(cmd string, cmds ...string) (string, error) {
+	command := append([]string{cmd}, cmds...)
+    c := exec.Command(vboxBin, command...)
+
+    output, err := c.CombinedOutput()
+
+    if err != nil {
+        return "", err
+    }
+
+    return string(output[:]), nil
+}
+
+
+func testCleanup(t *testing.T) func() {
+    return func() {
+        fmt.Println("Hello world!")
+        assert.Equal(t, nil, nil)
+    }
+}
+
+
+func TestVmBase(t *testing.T) {
+    // new vm
+    vm, err := vbox.NewVMFromOVA("go-ntp-ova.ova", "go-ntp")
+    assert.Equal(t, err, nil)
+
+    // check if it is created
+    cmd, err := execute("list", "vms")
+    assert.Equal(t, err, nil)
+    assert.Contains(t, cmd, "\"go-ntp\"") 
+
+    // start vm
+    err = vm.Start()
+    assert.Equal(t, err, nil)
+
+    // check if it is running
+    cmd, err = execute("list", "runningvms")
+    assert.Equal(t, err, nil)
+    assert.Contains(t, cmd, "\"go-ntp\"") 
+
+    // restart vm??
+    err = vm.Restart()
+    assert.Equal(t, err, nil)
+
+    // check if it is running
+    cmd, err = execute("list", "runningvms")
+    assert.Equal(t, err, nil)
+    assert.Contains(t, cmd, "\"go-ntp\"") 
+
+    // stop vm
+    err = vm.Stop()
+    assert.Equal(t, err, nil)
+
+    // check if it is running
+    cmd, err = execute("list", "runningvms")
+    assert.Equal(t, err, nil)
+    assert.NotContains(t, cmd, "\"go-ntp\"") 
+
+    // kill vm
+    err = vm.Kill()
+    assert.Equal(t, err, nil)
+
+    // check if it exists
+    cmd, err = execute("list", "vms")
+    assert.Equal(t, err, nil)
+    assert.NotContains(t, cmd, "\"go-ntp\"") 
+
+}
