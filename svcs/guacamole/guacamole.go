d348afd2501a1051f2e71568bda00d033f58953d
kdhageman <kdhageman@users.noreply.github.com>
2018-08-17 10:04:22 +0200

    Attempt to fix guacamole issue

--- svcs/guacamole/guacamole.go
+++ svcs/guacamole/guacamole.go
@@ -228,9 +228,9 @@ func (guac *guacamole) ConnectProxy(p revproxy.Proxy) error {
         proxy_pass http://{{.Host}}:8080/guacamole/;
         proxy_buffering off;
         proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection \$http_connection;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $http_connection;
         # proxy_cookie_path /guacamole/ /;
         access_log off;
     }`
