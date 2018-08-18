073cae9ac3c40563e62fe94ecc5ace1020144177
eyjhbb@gmail.com <eyjhbb@gmail.com>
2018-08-18 17:05:26 +0200

    cleanup: added clean_vms.sh to remove all virtual machines, and changed +x on both clean_*

---
+++ clean_vms.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VMS=$(vboxmanage list vms)
+
+while read -r line; do
+    line=$(echo $line | cut -d ' ' -f 2)
+    vms_id=$(echo $line | awk -F[{}] '{print $2}')
+    echo $vms_id
+		vboxmanage unregistervm $vms_id --delete
+done <<< "$VMS"
