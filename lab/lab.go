341d47e9fbb9b5847217a5fc1573fd45cd334aef
eyjhbb@gmail.com <eyjhbb@gmail.com>
2018-08-18 17:07:35 +0200

    lab: docker is no longer used as of previous commit

--- lab/lab.go
+++ lab/lab.go
@@ -3,7 +3,6 @@ package lab
 import (
 	"github.com/aau-network-security/go-ntp/exercise"
 	"github.com/aau-network-security/go-ntp/virtual"
-	"github.com/aau-network-security/go-ntp/virtual/docker"
 	"github.com/aau-network-security/go-ntp/virtual/vbox"
 	"github.com/rs/zerolog/log"
 )
