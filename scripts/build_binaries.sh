e6208ab05221c53b495761264b9251fea17da234
kdhageman <kdhageman@users.noreply.github.com>
2018-10-14 20:47:28 +0200

    Add cache and extension name to Windows binaries

--- scripts/build_binaries.sh
+++ scripts/build_binaries.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 GOARCH=( 386 amd64 )
+
 GOOS=( linux darwin windows )
+EXTENSION=( "" "" ".exe" )
 
 APPDIR=( daemon client )
 APPNAME=( ntpd ntp)
 
 for goarch in "${GOARCH[@]}"
 do
-    for goos in "${GOOS[@]}"
+    for j in "${!GOOS[@]}"
     do
+        goos="${GOOS[$j]}"
+        ext="${EXTENSION[$j]}"
         for i in "${!APPDIR[@]}"
         do
             appdir="${APPDIR[$i]}"
             appname="${APPNAME[$i]}"
-            env GOOS=$goos GOARCH=$goarch go build -o ./build/$appname-$goos-$goarch github.com/aau-network-security/go-ntp/app/$appdir
+            env GOOS=$goos GOARCH=$goarch go build -o ./build/$appname-$goos-$goarch$ext github.com/aau-network-security/go-ntp/app/$appdir
         done
     done
 done
