6a76c849b6f9e5d16cb483ef458bbf411777bd87
kdhageman <kdhageman@users.noreply.github.com>
2018-10-19 11:11:06 +0200

    Make script production ready

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,14 @@ f=./build/ntpd-linux-amd64
 user=ntpd
 hostname=sec02.lab.es.aau.dk
 keyfile=./travis_deploy_key
+deploy_path=/home/ntpd/daemon/ntpd
 
 if [ -f $f ]; then
     echo "Deploying '$f' to '$hostname'"
     chmod 600 $keyfile
-    ssh -i $keyfile $user@$hostname -o StrictHostKeyChecking=no pwd
+    ssh -i $keyfile -o StrictHostKeyChecking=no $user@$hostname sudo /bin/systemctl stop ntpd.service
+    scp -i $keyfile -o StrictHostKeyChecking=no $f $user@$hostname:$deploy_path
+    ssh -i $keyfile -o StrictHostKeyChecking=no $user@$hostname sudo /bin/systemctl start ntpd.service
 else
     echo "Error: $f does not exist"
     exit 1
