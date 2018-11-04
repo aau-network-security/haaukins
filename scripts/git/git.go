ddf27d9edd0a0eaa292fd3fa18e06083ab09a02d
kdhageman <kdhageman@users.noreply.github.com>
2018-11-04 10:58:31 +0100

    Add pushing to git

--- scripts/git/git.go
+++ scripts/git/git.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"github.com/coreos/go-semver/semver"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/config"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+	"os"
+	"os/user"
+	"path"
 	"time"
 )
 
@@ -70,10 +75,54 @@ func (r Repo) CreateBranch(version *semver.Version) error {
 	if err != nil {
 		return err
 	}
-	ref := plumbing.NewHashReference(referenceName(version.String()), headRef.Hash())
+	ref := plumbing.NewHashReference(referenceName(version), headRef.Hash())
 	return r.Repo.Storer.SetReference(ref)
 }
 
-func referenceName(branch string) plumbing.ReferenceName {
-	return plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch))
+func (r Repo) PushBranch() error {
+	refSpec, err := r.headRefSpec()
+	if err != nil {
+		return err
+	}
+
+	keyFile := os.Getenv("NTP_RELEASE_PEMFILE")
+	if keyFile == "" {
+		curUser, err := user.Current()
+		if err != nil {
+			return err
+		}
+		fmt.Println("Environment variable 'NTP_RELEASE_PEMFILE' is not defined, using default '~/.id_rsa'")
+		keyFile = path.Join(curUser.HomeDir, ".ssh", "id_rsa")
+	}
+	auth, err := ssh.NewPublicKeysFromFile("git", keyFile, "")
+	if err != nil {
+		return nil
+	}
+
+	po := &git.PushOptions{
+		RemoteName: "origin",
+		Auth:       auth,
+		RefSpecs: []config.RefSpec{
+			refSpec,
+		},
+	}
+	return r.Repo.Push(po)
+}
+
+func (r Repo) headRefSpec() (config.RefSpec, error) {
+	headRef, err := r.Repo.Head()
+	if err != nil {
+		return "", err
+	}
+
+	remote := headRef.Target()
+	if remote == "" {
+		remote = headRef.Name()
+	}
+
+	return config.RefSpec(fmt.Sprintf("%s:%s", headRef.Name(), remote)), nil
+}
+
+func referenceName(version *semver.Version) plumbing.ReferenceName {
+	return plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", version.String()))
 }
