3557a6408b50b60e5c5d6b7bfefc8479d633914d
kdhageman <kdhageman@users.noreply.github.com>
2018-11-05 10:06:47 +0100

    Revert "Fixed tests and added eventpool"
    This reverts commit 750d29326c1e709c1bf4355d3e670de32ad61beb.

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -439,7 +439,7 @@ func (d *daemon) CreateEvent(req *pb.CreateEventRequest, resp pb.Daemon_CreateEv
 	}
 
 	_, err := d.eventPool.GetEvent(evtag)
-	if err == nil {
+	if err != nil {
 		return DuplicateEventErr
 	}
 
