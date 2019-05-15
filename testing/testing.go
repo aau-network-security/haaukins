3b555f3260be3a66d039c8cc452028a35d3828ce
kdhageman <kdhageman@users.noreply.github.com>
2019-05-15 21:22:18 +0200

    Add license header to all .go files

--- testing/testing.go
+++ testing/testing.go
@@ -1,8 +1,12 @@
+// Copyright (c) 2018-2019 Aalborg University
+// Use of this source code is governed by a GPLv3
+// license that can be found in the LICENSE file.
+
 package testing
 
 import (
-	"testing"
 	"os"
+	"testing"
 )
 
 func SkipCI(t *testing.T) {
--- virtual/docker/pool_test.go
+++ virtual/docker/pool_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 Aalborg University
+// Use of this source code is governed by a GPLv3
+// license that can be found in the LICENSE file.
+
 package docker
 
 import (
