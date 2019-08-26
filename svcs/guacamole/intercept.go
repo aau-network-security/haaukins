d6c8679b9c175e7fd169319ce8528ff87a753cfa
mrturkmen06 <f.ahmet.turkmen@icloud.com>
2019-08-26 13:31:58 +0200

    replace waitinghtml

--- svcs/guacamole/intercept.go
+++ svcs/guacamole/intercept.go
@@ -14,216 +14,118 @@ import (
 )
 const waitingHTMLTemplate = `
 <html lang="en" dir="ltr">
-  <meta http-equiv="refresh" content="3" />
-  <head>
-    <style>
-    html,
-          body {
-            height: 100%;
-            width: 100%;
-            background-color: black;
-            }
-            #main-wrapper {
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            }
-            #box {
-            border-color: red;
-            border-style: solid;
-            border-width: 1px;
-            position: relative;
-            background-color: red;
-            width: 80%;
-            height: 80%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            }
-            #skull {
-            padding: 2vh;
-            padding-bottom: 0;
-            position: relative;
-            z-index: 3;
-            }
-            .warning-msg {
-            position: relative;
-            padding: 1.5vw;
-            color: white;
-            font-weight: bold;
-            font-size: 5vw;
-            z-index: 4;
-            font-family: 'VT323', monospace;
-            letter-spacing: 0.5em;
-            }
-            .background-bars-wrapper {
-            position: absolute;
-            display: flex;
-            width: 100%;
-            height: 50%;
-            }
-            #bottom-background-bars-wrapper {
-            align-items: flex-end;
-            bottom: 0;
-            left: 0;
-            z-index: 1;
-            }
-            #top-background-bars-wrapper {
-            align-items: flex-start;
-            top: 0;
-            left: 0;
-            z-index: 2;
-            }
-            .bar1 {
-            animation-delay: 0.1s;
-            }
-            .bar2 {
-            animation-delay: 0.2s;
-            }
-            .bar3 {
-            animation-delay: 0.3s;
-            }
-            .bar4 {
-            animation-delay: 0.4s;
-            }
-            .bar5 {
-            animation-delay: 0.5s;
-            }
-            .bar6 {
-            animation-delay: 0.5s;
-            }
-            .bar7 {
-            animation-delay: 0.4s;
-            }
-            .bar8 {
-            animation-delay: 0.3s;
-            }
-            .bar9 {
-            animation-delay: 0.2s;
-            }
-            .bar10 {
-            animation-delay: 0.1s;
-            }
-            .background-bar {
-            animation-timing-function: ease;
-            animation-duration: 0.4s;
-            animation-name: bar;
-            animation-iteration-count: infinite;
-            animation-direction: alternate;
-            height: 50%;
-            width: 10%;
-            margin-left: 1px;
-            margin-right: 1px;
-            background-color: black;
-            }
-            @-webkit-keyframes bar {
-            from {
-              height: 50%;
-            }
-            to {
-              height: 90%;
-            }
-            }
-            @keyframes bar {
-            from {
-              height: 50%;
-            }
-            to {
-              height: 90%;
-            }
-            }
+		  <meta http-equiv="refresh" content="3" />
+		  <head>
+			<style>
+				html, body {
+		  height: 100%;
+		  width: 100%;
+		  margin: 0;
+		  padding: 0;
+		  font-size: 100%;
+		  background: #191a1a;
+		  text-align: center;
+		}
+		
+		h1 {
+		  margin: 100px;
+		  padding: 0;
+		  font-family: ‘Arial Narrow’, sans-serif;
+		  font-weight: 100;
+		  font-size: 1.1em;
+		  color: #a3e1f0;
+		}
+		h2 {
+		  margin:50px;
+		  color: #a3e1f0;
+		  font-family: ‘Arial Narrow’, sans-serif;
+		}
+		
+		span {
+		  position: relative;
+		  top: 0.63em;  
+		  display: inline-block;
+		  text-transform: uppercase;  
+		  opacity: 0;
+		  transform: rotateX(-90deg);
+		}
+		
+		.let1 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.2s;
+		}
+		
+		.let2 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.3s;
+		}
+		
+		.let3 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.4s;
+		}
+		
+		.let4 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.5s;
+		
+		}
+		
+		.let5 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.6s;
+		}
+		
+		.let6 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.7s;
+		}
+		
+		.let7 {
+		  animation: drop 1.2s ease-in-out infinite;
+		  animation-delay: 1.8s;
+		}
+		
+		@keyframes drop {
+			10% {
+				opacity: 0.5;
+			}
+			20% {
+				opacity: 1;
+				top: 3.78em;
+				transform: rotateX(-360deg);
+			}
+			80% {
+				opacity: 1;
+				top: 3.78em;
+				transform: rotateX(-360deg);
+			}
+			90% {
+				opacity: 0.5;
+			}
+			100% {
+				opacity: 0;
+				top: 6.94em
+			}
+		}
+
     </style>
   </head>
   <body>
+  <h1>
+    <span class="let1">l</span>  
+    <span class="let2">o</span>  
+    <span class="let3">a</span>  
+    <span class="let4">d</span>  
+    <span class="let5">i</span>  
+    <span class="let6">n</span>  
+    <span class="let7">g</span>  
+  </h1>
+<h2>
+Virtualized Environment
 
-<div id="main-wrapper">
-  <div id="box">
+</h2>
 
-    <div id="top-background-bars-wrapper" class="background-bars-wrapper">
-      <div class="bar1 background-bar"></div>
-      <div class="bar2 background-bar"></div>
-      <div class="bar3 background-bar"></div>
-      <div class="bar4 background-bar"></div>
-      <div class="bar5 background-bar"></div>
-      <div class="bar6 background-bar"></div>
-      <div class="bar7 background-bar"></div>
-      <div class="bar8 background-bar"></div>
-      <div class="bar9 background-bar"></div>
-      <div class="bar10 background-bar"></div>
-    </div>
-
-    <div id="bottom-background-bars-wrapper" class="background-bars-wrapper">
-      <div class="bar1 background-bar"></div>
-      <div class="bar2 background-bar"></div>
-      <div class="bar3 background-bar"></div>
-      <div class="bar4 background-bar"></div>
-      <div class="bar5 background-bar"></div>
-      <div class="bar6 background-bar"></div>
-      <div class="bar7 background-bar"></div>
-      <div class="bar8 background-bar"></div>
-      <div class="bar9 background-bar"></div>
-      <div class="bar10 background-bar"></div>
-    </div>
-
-    <svg id="skull" viewbox="0 0 500 565" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-
-            <filter id="softGlow" height="300%" width="400%" x="-75%" y="-75%">
-
-              <feMorphology operator="dilate" radius="5" in="SourceAlpha" result="thicken" />
-              <feGaussianBlur in="thicken" stdDeviation="5" result="blurred" />
-              <feFlood flood-color="rgb(255,0,0)" result="glowColor" />
-              <feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />
-
-              <feMerge>
-                <feMergeNode in="softGlow_colored"/>
-                <feMergeNode in="SourceGraphic"/>
-              </feMerge>
-
-            </filter>
-
-          </defs>
-
-          <g transform="translate(50 330)">
-          <path fill="white" stroke="black" stroke-width="20" filter="url(#softGlow)"
-                                             d="
-                                             M 0 0
-                                             a 215 215 0 1 1 400 0
-                                             v 50
-                                             h 20
-                                             v 90
-                                             l -20 20
-                                             h -80
-                                             v -30
-                                             v 70
-                                             h -240
-                                             v -70
-                                             v 30
-                                             h -80
-                                             l -20 -20
-                                             v -90
-                                             h 20
-                                             z"/>
-          <circle id="left-eye" fill="red" stroke="black" stroke-width="20"
-                                                          cx="100" cy="0" r="50" />
-          <use href="#left-eye" x="200" y="0" />
-
-          <path id="nose" fill="none" stroke="black" stroke-width="20"
-                                                     d="
-                                                     M 200 50
-                                                     v 40" />
-          <use id="tooth" href="#nose" x="-90" y="100" />
-          <use href="#tooth" x="40" y="0" />
-          <use href="#tooth" x="90" y="0" />
-          <use href="#tooth" x="140" y="0" />
-          <use href="#tooth" x="180" y="0" />
-          </g>
-        </svg>
-    <div class="warning-msg">Connecting</div>
-  </div>
-</div>
   </body>
 </html>
 
