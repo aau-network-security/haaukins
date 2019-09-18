12cbb598f89cdfed86019d13304bdc53c64368b0
mrturkmen06 <f.ahmet.turkmen@icloud.com>
2019-09-18 09:49:03 +0200

    malformed docker endpoint string fixed

--- exercise/environment_test.go
+++ exercise/environment_test.go
@@ -37,7 +37,7 @@ func TestBasicEnvironment(t *testing.T) {
 		},
 	}
 
-	dclient, err := docker.NewClient("sud")
+	dclient, err := docker.NewClient("unix:///var/run/docker.sock")
 	if err != nil {
 		t.Fatalf("unable to access docker environment: %s", err)
 	}
