25e46112b64bc48e83d05c2079a11d51b23db7fa
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2019-12-10 14:39:29 +0100

    Modified Proto for Web Client

--- app/client/cli/event.go
+++ app/client/cli/event.go
@@ -155,7 +155,7 @@ func (c *Client) CmdEvents() *cobra.Command {
 
 			f := formatter{
 				header: []string{"EVENT TAG", "NAME", "# TEAM", "# EXERCISES", "CAPACITY", "CREATION TIME"},
-				fields: []string{"Tag", "Name", "TeamCount", "ExerciseCount", "Capacity", "CreationTime"},
+				fields: []string{"Tag", "Name", "TeamCount", "Exercises", "Capacity", "CreationTime"},
 			}
 
 			var elements []formatElement
