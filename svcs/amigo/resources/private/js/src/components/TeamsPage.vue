cd112383349725829d8b464e04faa8ba4a935017
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2020-04-18 12:44:08 +0200

    removed dummy data

--- svcs/amigo/resources/private/js/src/components/TeamsPage.vue
+++ svcs/amigo/resources/private/js/src/components/TeamsPage.vue
@@ -9,12 +9,12 @@
                     <th>Score</th>
                 </tr>
             </thead>
-            <tbody v-if="dummy.length > 0">
-            <tr v-for="(team, index) in dummy" v-bind:key="team.id">
-                <td>{{index}}</td>
-                <td>{{team.name}}</td>
-                <td>{{team.tpoints}}</td>
-            </tr>
+            <tbody v-if="teams.length > 0">
+                <tr v-for="(team, index) in teams" v-bind:key="team.id">
+                    <td>{{index}}</td>
+                    <td>{{team.name}}</td>
+                    <td>{{team.tpoints}}</td>
+                </tr>
             </tbody>
             <tbody v-else>
                 <tr class="text-center"><td colspan="3">No team registered to this event!</td></tr>
@@ -33,10 +33,7 @@
         },
         data: function () {
             return {
-                dummy: [
-                    { "id": "227e9de3", "name": "test", "tpoints": 22, "completions": [ "2020-03-14T12:30:15.480666371+01:00", "2020-03-14T12:30:35.295348636+01:00" ], "points": [ 7, 15 ], "is_user": false },
-                    { "id": "dd633565", "name": "menne", "tpoints": 0, "completions": [ "2020-03-13T12:30:15.480666371+01:00", null ], "points": [ 7, 15 ], "is_user": false },
-                    { "id": "4e10b5c1", "name": "merlo", "tpoints": 0, "completions": [ null, "2020-03-12T12:30:15.480666371+01:00" ], "points": [ 7, 15 ], "is_user": true } ],
+                teams: [],
                 data: [{
                     x: [1, 2, 3, 4],
                     y: [10, 15, 13, 17],
@@ -63,35 +60,57 @@
             }
         },
         created() {
-            this.sortSolvedData()
-            this.scoreGraph()
+            let url = new URL('/challengesFrontend', window.location.href);
+            url.protocol = url.protocol.replace('http', 'ws');
+            this.connectToWS(url.href);
         },
         methods: {
+            connectToWS: function(url) {
+                let self = this;
+                let ws = new WebSocket(url);
+                ws.onmessage = self.receiveMsg;
+                ws.onclose = function(){
+                    ws = null;
+                    setTimeout(function(){self.connectToWS(url)}, 3000);
+                };
+            },
+            receiveMsg: function(evt) {
+                let messages = evt.data.split('\n');
+                for (let i = 0; i < messages.length; i++) {
+                    let msg = messages[i];
+                    let json = JSON.parse(msg);
+                    if (json.msg === "scoreboard"){
+                        this.teams = json.values.teams;
+                        this.sortSolvedData()
+                    }
+                }
+            },
             sortSolvedData: function(){
-                for (let i=0; i < this.dummy.length; i++){
+                for (let i=0; i < this.teams.length; i++){
 
                     let list = [];
-                    for (let j = 0; j < this.dummy[i].completions.length; j++)
-                        list.push({'date': this.dummy[i].completions[j], 'points': this.dummy[i].points[j]});
+                    for (let j = 0; j < this.teams[i].completions.length; j++)
+                        list.push({'date': this.teams[i].completions[j], 'points': this.teams[i].points[j]});
 
                     list.sort((a, b) => new Date(a.date) - new Date(b.date) )
 
                     for (let k = 0; k < list.length; k++) {
-                        this.dummy[i].completions[k] = list[k].date;
-                        this.dummy[i].points[k] = list[k].points;
+                        this.teams[i].completions[k] = list[k].date;
+                        this.teams[i].points[k] = list[k].points;
                     }
                 }
+                this.scoreGraph()
             },
             scoreGraph: function () {
 
-                for (let i=0; i < this.dummy.length; i++){
+                for (let i=0; i < this.teams.length; i++){
                     let team_score = [];
                     let times = [];
-                    for(let j = 0; j < this.dummy[i].completions.length; j++){
-                        if (this.dummy[i].completions[j] != null){
-                            let date = new Date(this.dummy[i].completions[j])
+                    for(let j = 0; j < this.teams[i].completions.length; j++){
+                        if (this.teams[i].completions[j] != null){
+                            let date = new Date(this.teams[i].completions[j])
                             times.push(date)
-                            team_score.push(this.dummy[i].points[j])
+                            team_score.push(this.teams[i].points[j])
                         }
                     }
 
@@ -100,12 +119,12 @@
                         x: times,
                         y: team_score,
                         mode: 'lines+markers',
-                        name: this.dummy[i].name,
+                        name: this.teams[i].name,
                         marker: {
-                            color: this.colorHash(this.dummy[i].name + this.dummy[i].id),
+                            color: this.colorHash(this.teams[i].name + this.teams[i].id),
                         },
                         line: {
-                            color: this.colorHash(this.dummy[i].name + this.dummy[i].id),
+                            color: this.colorHash(this.teams[i].name + this.teams[i].id),
                         }
                     };
                     this.traces.push(trace);
@@ -118,9 +137,6 @@
                     }
                     return scorediff;
                 });
-                window.console.log(this.traces);
-
-
             },
             cumulativeSum: function (arr) {
                 let result = arr.concat();
