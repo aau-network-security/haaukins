86e11f385ac71f02b3e02b51f0b811df46c9dd5e
Ahmet Türkmen <f.ahmet.turkmen@icloud.com>
2020-11-25 14:48:50 +0100

    Create set-routing.sh

---
+++ scripts/set-routing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+# First stop nftables if any 
+systemctl stop nftables
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8081
