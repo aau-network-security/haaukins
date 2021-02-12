8086b5a7c34b75da7593d7b0107862ca831cc47a
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2021-02-12 10:24:35 +0300

    optimize bundle files through webpack
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- svcs/amigo/resources/private/js/vue.config.js
+++ svcs/amigo/resources/private/js/vue.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports =  {
     chainWebpack: config => {
         if(config.plugins.has('extract-css')) {
             const extractCSSPlugin = config.plugin('extract-css')
@@ -8,10 +10,36 @@ module.exports = {
             }])
         }
     },
-    configureWebpack: {
-        output: {
-            filename: '[name].js',
-            chunkFilename: '[name].js'
+    configureWebpack: config => {
+        config.optimization = {
+            splitChunks: {
+                chunks: 'all',
+                minSize: 10000,
+                maxSize: 250000,
+            },
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        ecma: undefined,
+                        warnings: false,
+                        parse: {},
+                        compress: { drop_console: true },
+                        mangle: true, // Note `mangle.properties` is `false` by default.
+                        module: false,
+                        output: { comments: false },
+                        toplevel: false,
+                        nameCache: null,
+                        ie8: false,
+                        keep_classnames: undefined,
+                        keep_fnames: false,
+                        safari10: false,
+                    },
+                }),
+            ],
+
         }
+        config.devtool = false
+        config.mode = process.env.NODE_ENV
     }
 }
