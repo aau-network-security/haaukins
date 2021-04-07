8dd3f38fb032cef62b0ba475976e6dbeedbc5014
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2021-04-07 13:24:09 +0300

    break in case event exceeds 253 VPN connections
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- svcs/guacamole/event.go
+++ svcs/guacamole/event.go
@@ -380,12 +380,18 @@ func (ev *event) CreateVPNConn(t *store.Team, labInfo *labNetInfo) ([]string, er
 	}
 
 	// create 4 different config file for 1 user
-
+	var ipAddr int
 	// todo: temporary changed 1.
 	for i := 240; i < 244; i++ {
 
 		// generate client privatekey
-		ipAddr := pop(&ev.ipAddrs)
+		if len(ev.ipAddrs) != 0 {
+			ipAddr = pop(&ev.ipAddrs)
+		} else {
+			log.Error().Msgf("Max number of connections reached on VPN connection %d", 252)
+			break
+		}
+
 		log.Info().Msgf("Generating privatekey for team %s", evTag+"_"+teamID)
 		_, err = ev.wg.GenPrivateKey(ctx, &wg.PrivKeyReq{PrivateKeyName: evTag + "_" + teamID + "_" + strconv.Itoa(ipAddr)})
 		if err != nil {
